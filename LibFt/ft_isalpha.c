86d9d63bfcb16f9c4e6751f0a2ea708c59ff8fcc
AlexisNoukan <alexisnoukan@gmail.com>
2023-10-18 11:38:54 +0200

    ft_isalpha complete

--- LibFt/ft_isalpha.c
+++ LibFt/ft_isalpha.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
 int  ft_isalpha(char str)
 {
-  int  i;
-
-  i = 0;
-  if (str == `\0`)
+  if (str == '\0')
     return(1);
-  if (!((str >= 'A' && str <= 'Z') || \
-				(str >= 'a' && str <= 'z')))
-    return(0);
-  return(1);
+  if (!((str >= 'A' && str <= 'Z') || (str >= 'a' && str <= 'z')))
+    return(1);
+  return(0);
 }
 
 int main()
 {
-  char  c;
-  int   i;
-  c = "C";
-  i = ft_isalpha(c); 
+  char  c = 'C';
+  int i = ft_isalpha(c);
   printf("%d", i);
-
 }
+
 
 
