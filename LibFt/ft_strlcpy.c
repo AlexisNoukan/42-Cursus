12d4ceed79ff1951e8ee4068df8b15aeff03823c
AlexisNoukan <alexisnoukan@gmail.com>
2023-10-19 09:01:14 +0200

    save

--- LibFt/ft_strlcpy.c
+++ LibFt/ft_strlcpy.c
@@ -1,23 +1,29 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
-#                                                     +:+ +:+         +:+      #
-#    By: anoukan <marvin@42.fr>                     +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2023/10/16 13:14:31 by anoukan           #+#    #+#              #
-/*   Updated: 2023/10/16 14:09:18 by anoukan          ###   ########.fr       */
-#                                                                              #
-# **************************************************************************** #
-char	ft_strcpy(char *dest, char *str, int l)
-{
-	int	i;
+#include "stdio.h"
+#include "string.h"
+#include <stddef.h>
+size_t	ft_strlcpy(char *dest, char const *str, size_t l)
 
-  i = 0;
-	while(str[i] != '\0' || i != l)
+{
+	size_t	i;
+
+	l = -1;
+	i = 0;
+	while (str[i] != '\0' && i < l - 1)
 	{
-		str[i] == dest[i];
+		dest[i] = str[i];
 		++i;
 	}
-	return (dest);
+	dest[i] = '\0';
+	return (i);
+}
+
+int	main()
+{
+	char	s[] = "lala";
+	char	d[5] = "lolo";
+	int	l = 4;
+
+	printf("%zu\n", strlcpy(d, s, l));
+	printf("%zu\n", ft_strlcpy(d, s, l));
+	return (0);
 }
---
+++ LibFt/ft_strnstr.c
@@ -0,0 +1,16 @@
+char	*ft_strnstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+	int	n;
+
+	i = 0;
+	while (str[i])
+	{
+		n = i;
+		j = 0;
+		if (to_find[j] = '\0')
+			return (str + i );
+		
+	}
+}
