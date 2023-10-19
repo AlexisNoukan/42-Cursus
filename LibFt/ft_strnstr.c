732159676b9cd413e7c153eb4b9eb3dfa1008c82
AlexisNoukan <alexisnoukan@gmail.com>
2023-10-19 13:06:25 +0200

    ft_strncmp complete

--- LibFt/ft_strnstr.c
+++ LibFt/ft_strnstr.c
@@ -1,16 +1,48 @@
-char	*ft_strnstr(char *str, char *to_find)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anoukan <anoukan@student.42mulhouse.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 13:06:01 by anoukan           #+#    #+#             */
+/*   Updated: 2023/10/19 13:06:06 by anoukan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stdio.h"
+#include "string.h"
+
+char	*ft_strnstr(char *str, char *to_find, size_t len)
 {
-	int	i;
-	int	j;
-	int	n;
+	size_t	i;
+	size_t	j;
+	size_t	n;
 
 	i = 0;
-	while (str[i])
+	while (i < len)
 	{
 		n = i;
 		j = 0;
-		if (to_find[j] = '\0')
-			return (str + i );
-		
+		if (to_find[j] == '\0')
+			return (str);
+		while (str[n] == to_find[j] && str[n] && to_find[j] && n < len)
+		{
+			n++;
+			j++;
+		}
+		if (to_find[j] == '\0')
+			return (str + i);
+		++i;
 	}
+	return (NULL);
+}
+
+int	main()
+{
+	char	s1[] = "lolHelloWorldloo";
+	char	s2[] = "HelloWorld";
+	size_t len = 8;
+	printf("ft_strnstr : %s\n", ft_strnstr(s1, s2, len));
+	printf("strnstr : %s", strnstr(s1, s2, len));
 }
