c4f785d6b8fb86aa2d86a752d235349b4339e812
AlexisNoukan <alexisnoukan@gmail.com>
2023-10-23 14:04:45 +0200

    ft_strchr complete

--- LibFt/ft_strchr.c
+++ LibFt/ft_strchr.c
@@ -6,11 +6,24 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 0009/10/20 17:39:49 by noukan            #+#    #+#             */
-/*   Updated: 2023/10/23 11:48:44 by anoukan          ###   ########.fr       */
+/*   Updated: 23/10/2023 14:04:24 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char    *ft_strchr(const char *s, int c)
 {
+    int     i;
+    char    *str;
 
+    i = 0;
+    str = (char *)s;
+    if (c == '\0')
+        return(str + ft_strlen(str));
+    while(str[i])
+    {
+        if (str[i] == c)
+            return (&str[i]);
+        i++;
+    }
+    return (0);
 }
