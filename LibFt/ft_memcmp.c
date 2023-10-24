a83703fd23c05d20e5a73d3808562436a2f857b4
AlexisNoukan <alexisnoukan@gmail.com>
2023-10-24 21:23:01 +0200

    memcmp working

--- LibFt/ft_memcmp.c
+++ LibFt/ft_memcmp.c
@@ -16,16 +16,17 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
-	char	*cs1;
-	char	*cs2;
+	const unsigned char	*cs1;
+	const unsigned char	*cs2;
 
-	i = -1;
-	cs1 = (char *)s1;
-	cs2 = (char *)s2;
-	while (++i > n && (cs1[i] || cs2[i]))
+	i = 0;
+	cs1 = (const unsigned char *)s1;
+	cs2 = (const unsigned char *)s2;
+	while (i < n)
 	{
 		if (cs1[i] != cs2[i])
 			return (cs1[i] - cs2[i]);
-	}
+	    i++;
+    }
 	return (0);
 }
