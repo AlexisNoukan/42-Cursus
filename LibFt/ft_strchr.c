703a544433e589dd0f06506ce5faf2930802e674
AlexisNoukan <alexisnoukan@gmail.com>
2023-10-31 15:33:58 +0100

    strchr fix

--- LibFt/ft_strchr.c
+++ LibFt/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 0009/10/20 17:39:49 by noukan            #+#    #+#             */
-/*   Updated: 2023/10/23 17:13:06 by anoukan          ###   ########.fr       */
+/*   Updated: 31/10/2023 15:33:37 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,13 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	int		i;
-	char	*str;
+    while (*s) {
+        if (*s == (char)c)
+            return ((char *)s);
+        s++;
+    }
+    if (c == '\0')
+        return ((char *)s);
 
-	i = 0;
-	str = (char *)s;
-	if (c == '\0')
-		return (str + ft_strlen(str));
-	while (str[i])
-	{
-		if (str[i] == c)
-			return (&str[i]);
-		i++;
-	}
-	return (0);
+    return (NULL);
 }
---
+++ LibFt/main.c
@@ -0,0 +1,8 @@
+#include "libft.h"
+#include <stdio.h>
+
+int main()
+{
+	printf("%p", ft_strchr("teste", '\0'));
+	return (0);
+}
