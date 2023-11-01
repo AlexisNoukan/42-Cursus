ae52119915a343eae328e1bde7690b3ad0942015
AlexisNoukan <alexisnoukan@gmail.com>
2023-11-01 15:01:45 +0100

    calloc protection

--- LibFt/ft_calloc.c
+++ LibFt/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 0003/10/20 17:26:08 by noukan            #+#    #+#             */
-/*   Updated: 2023/11/01 06:49:37 by anoukan          ###   ########.fr       */
+/*   Updated: 2023/11/01 14:48:48 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*i;
 
+	if (count * size > INT_MAX || count > INT_MAX || size > INT_MAX)
+		return (NULL);
 	i = malloc(count * size);
 	if (i)
 		ft_bzero(i, count * size);
