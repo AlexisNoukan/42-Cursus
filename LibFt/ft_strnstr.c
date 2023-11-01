320401d0806386e7dcc260b86802f334c53778c6
AlexisNoukan <alexisnoukan@gmail.com>
2023-11-01 09:11:16 +0100

    norm

--- LibFt/ft_strnstr.c
+++ LibFt/ft_strnstr.c
@@ -6,25 +6,19 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:06:01 by anoukan           #+#    #+#             */
-/*   Updated: 2023/11/01 06:47:11 by anoukan          ###   ########.fr       */
+/*   Updated: 2023/11/01 09:11:09 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *str, const char *to_find, size_t len)
+char	*ft_strnstr_2(char *cstr, char *cto_find, size_t len)
 {
 	size_t	i;
 	size_t	j;
 	size_t	n;
-	char	*cstr;
-	char	*cto_find;
 
 	i = 0;
-	cstr = (char *)str;
-	cto_find = (char *)to_find;
-	if (cto_find[0] == '\0')
-		return (cstr);
 	while (i < len && cstr[i] != '\0')
 	{
 		if (!cstr)
@@ -41,4 +35,12 @@ char	*ft_strnstr(const char *str, const char *to_find, size_t len)
 		i++;
 	}
 	return (NULL);
+}
+
+char	*ft_strnstr(const char *str, const char *to_find, size_t len)
+{
+	if (to_find[0] == '\0')
+		return ((char *)str);
+	else
+		return (ft_strnstr_2((char *)str, (char *)to_find, len));
 }
