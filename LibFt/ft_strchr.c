9b114c91d3952780a4c75166206b8d058895cc71
AlexisNoukan <alexisnoukan@gmail.com>
2023-11-01 13:44:12 +0100

    strchr working

--- LibFt/ft_strchr.c
+++ LibFt/ft_strchr.c
@@ -14,13 +14,14 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s)
-	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
-	}
-	if (c == '\0')
-		return ((char *)s);
-	return (NULL);
+    int i;
+
+    i = 0;
+    while (s[i] != (unsigned char)c && s[i])
+        ++i;
+    if (s[i] == (unsigned char)c)
+        return ((char *)&s[i]);
+    else
+        return (NULL);
 }
+
