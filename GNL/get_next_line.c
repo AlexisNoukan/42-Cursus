ab282e3af7016fac7369b9243c648d99b9ae4fb0
AlexisNoukan <alexisnoukan@gmail.com>
2024-01-12 09:38:44 +0100

    norm

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,94 +6,94 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 12:20:10 by anoukan           #+#    #+#             */
-/*   Updated: 2024/01/12 08:56:45 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/01/12 09:38:38 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char    *ft_clean(char *stash)
+char	*ft_clean(char *stash)
 {
-    int     i;
-    char    *tmp;
+	int		i;
+	char	*tmp;
 
-    tmp = malloc(ft_strlen(stash) + 1);
-    if (!tmp)
-        return NULL;
-    i = 0;
-    while (stash[i] != '\n' && stash[i] != '\0')
-        i++;
-    if (stash[i] == '\n')
-        i++;
-    ft_strncpy(tmp, stash + i, ft_strlen(stash + i));
-    tmp[ft_strlen(stash + i)] = '\0';
-    free(stash);
-    stash = NULL;
-    return tmp;
+	tmp = malloc(ft_strlen(stash) + 1);
+	if (!tmp)
+		return (NULL);
+	i = 0;
+	while (stash[i] != '\n' && stash[i] != '\0')
+		i++;
+	if (stash[i] == '\n')
+		i++;
+	ft_strncpy(tmp, stash + i, ft_strlen(stash + i));
+	tmp[ft_strlen(stash + i)] = '\0';
+	free(stash);
+	stash = NULL;
+	return (tmp);
 }
 
-char    *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static char *stash;
-    char        *line;
-    int         bytes_read;
-    size_t      len;
-    char        *result;
+	static char	*stash;
+	char		*line;
+	int			bytes_read;
+	size_t		len;
+	char		*result;
 
-    //Allocation de memmoir et verification fichier vide
-    line = malloc((BUFFER_SIZE + 1) * sizeof(char));
-    if (!line)
-        return NULL;
-    bytes_read = read(fd, line, BUFFER_SIZE);
-    if (bytes_read <= 0)
-    {
-        free(line);
-        line = NULL;
-        return NULL;
-    }
-    //allocation de la stash si elle n existe pas ainsi que stockage de la premiere iteration du programme
-    if (!stash)
-    {
-        stash = malloc(sizeof(char));
-        if (!stash)
-        {
-            free(line);
-            return NULL;
-        }
-        *stash = '\0';
-        stash = ft_strjoin(stash, line);
-    }  
-    //verification et stockage dans la stash
-    while (!ft_verify(stash))
-    {
-        bytes_read = read(fd, line, BUFFER_SIZE);
-        if (bytes_read <= 0)
-            break;
-        line[bytes_read] = '\0';
-        stash = ft_strjoin(stash, line);
-    }
-    //trouver la len a ecrire
-    len = 0;
-    while (stash[len] != '\n' && stash[len] != '\0')
-        len++;
-    //verification fichier contenant uniquement un \n
-    if (stash[0] == '\n' && len == 0)
-    {
-        free(line);
-        free(stash);
-        return NULL;
-    }
-    //allocation du resultat
-    result = malloc((len + 1) * sizeof(char));
-    if (!result)
-    {
-        free(line);  
-        return NULL;
-    }
-    //copie de la ligne dans le resultat et cleaning de la stash
-    ft_strncpy(result, stash, len);
-    result[len] = '\0';
-    stash = ft_clean(stash);
-    free(line);
-    return result;
-} 
+	// Allocation de memmoir et verification fichier vide
+	line = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!line)
+		return (NULL);
+	bytes_read = read(fd, line, BUFFER_SIZE);
+	if (bytes_read <= 0)
+	{
+		free(line);
+		line = NULL;
+		return (NULL);
+	}
+	// allocation de la stash si elle n existe pas ainsi que stockage de la premiere iteration du programme
+	if (!stash)
+	{
+		stash = malloc(sizeof(char));
+		if (!stash)
+		{
+			free(line);
+			return (NULL);
+		}
+		*stash = '\0';
+		stash = ft_strjoin(stash, line);
+	}
+	// verification et stockage dans la stash
+	while (!ft_verify(stash))
+	{
+		bytes_read = read(fd, line, BUFFER_SIZE);
+		if (bytes_read <= 0)
+			break ;
+		line[bytes_read] = '\0';
+		stash = ft_strjoin(stash, line);
+	}
+	// trouver la len a ecrire
+	len = 0;
+	while (stash[len] != '\n' && stash[len] != '\0')
+		len++;
+	// verification fichier contenant uniquement un \n
+	if (stash[0] == '\n' && len == 0)
+	{
+		free(line);
+		free(stash);
+		return (NULL);
+	}
+	// allocation du resultat
+	result = malloc((len + 1) * sizeof(char));
+	if (!result)
+	{
+		free(line);
+		return (NULL);
+	}
+	// copie de la ligne dans le resultat et cleaning de la stash
+	ft_strncpy(result, stash, len);
+	result[len] = '\0';
+	stash = ft_clean(stash);
+	free(line);
+	return (result);
+}
--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 12:20:36 by anoukan           #+#    #+#             */
-/*   Updated: 2024/01/11 09:50:03 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/01/12 09:38:21 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,23 @@
 
 # include <stdio.h>
 //# include <stdarg.h>
-# include <unistd.h>
-# include <stdlib.h>
 # include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-//Buffer
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
+// Buffer
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 // base function
-char 	*get_next_line(int fd);
+char	*get_next_line(int fd);
 
 // utils function
 int		ft_strlen(char const *s);
 char	*ft_strncpy(char *dest, char *src, unsigned int n);
 char	*ft_strjoin(char *s1, char *s2);
-int	    ft_verify(char *stash);
+int		ft_verify(char *stash);
 void	ft_line(char *stash, char *line);
 char	*ft_clean(char *stash);
 
