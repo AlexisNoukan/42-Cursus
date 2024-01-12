fa09b51f93d380a9ed9c697e1e214d98ae6675f2
AlexisNoukan <alexisnoukan@gmai.com>
2024-01-12 15:42:15 +0100

    remove comment and clean

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 12:20:10 by anoukan           #+#    #+#             */
-/*   Updated: 2024/01/12 15:39:29 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/01/12 15:41:47 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,10 @@ char	*ft_clean(char *stash)
 	return (tmp);
 }
 
-char	*get_next_line_2(char *stash, char *buffer, int bytes_read)
+char	*get_next_line_2(char *stash, char *buffer, int bytes_read, int fd)
 {
 	char	*result;
+    size_t     len;
 
 	while (!ft_verify(stash))
 	{
@@ -44,15 +45,12 @@ char	*get_next_line_2(char *stash, char *buffer, int bytes_read)
 		buffer[bytes_read] = '\0';
 		stash = ft_strjoin(stash, buffer);
 	}
-	// trouver la len a ecrire + creer une fonction avec tous ca
 	len = 0;
 	while (stash[len] != '\n' && stash[len] != '\0')
 		len++;
-	// allocation du resultat
 	result = malloc((len + 1) * sizeof(char));
 	if (!result)
 		return (free(stash), free(buffer), NULL);
-	// copie de la ligne dans le resultat et cleaning de la stash
 	ft_strncpy(result, stash, len);
 	result[len] = '\0';
 	stash = ft_clean(stash);
@@ -65,9 +63,7 @@ char	*get_next_line(int fd)
 	static char	*stash;
 	char		*buffer;
 	int			bytes_read;
-	size_t		len;
 
-	/*Allocation de memmoir et verification fichier vide*/
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
 		return (NULL);
@@ -75,7 +71,6 @@ char	*get_next_line(int fd)
 	buffer[bytes_read] = '\0';
 	if (bytes_read < 0)
 		return (free(buffer), buffer = NULL, NULL);
-	// allocation de la stash si elle n existe pas ainsi que stockage de la premiere iteration du programme
 	if (!stash)
 	{
 		stash = malloc(sizeof(char));
@@ -84,10 +79,7 @@ char	*get_next_line(int fd)
 		*stash = '\0';
 	}
 	stash = ft_strjoin(stash, buffer);
-	// securite fichier vide
 	if (stash[0] == '\0')
 		return (free(stash), stash = NULL, free(buffer), buffer = NULL, NULL);
-	// verification et stockage dans la stash
-	+ajouter ca a une fonction de verification return (get_next_line_2(stash,
-			buffer, bytes_read));
+    return (get_next_line_2(stash, buffer, bytes_read, fd));
 }
