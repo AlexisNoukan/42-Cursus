64b22752f1ec54dd147006b09423b0b7ca5bc804
AlexisNoukan <alexisnoukan@gmail.com>
2024-01-22 13:53:46 +0100

    h

--- so_long/main.c
+++ so_long/main.c
@@ -6,7 +6,7 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 11:45:10 by anoukan           #+#    #+#             */
-/*   Updated: 2024/01/22 11:54:38 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/01/22 13:53:39 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,40 +17,39 @@
 // See README in the root project for more information.
 // -----------------------------------------------------------------------------
 
-
 #define WIDTH 512
 #define HEIGHT 512
 
-static mlx_image_t* image;
+static mlx_image_t	*image;
 
 // -----------------------------------------------------------------------------
 
-int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+int32_t	ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
 {
-    return (r << 24 | g << 16 | b << 8 | a);
+	return (r << 24 | g << 16 | b << 8 | a);
 }
 
-void ft_randomize(void* param)
+void	ft_randomize(void *param)
 {
 	for (int32_t i = 0; i < image->width; ++i)
 	{
 		for (int32_t y = 0; y < image->height; ++y)
 		{
-			uint32_t color = ft_pixel(
-				rand() % 0xFF, // R
-				rand() % 0xFF, // G
-				rand() % 0xFF, // B
-				rand() % 0xFF  // A
+			uint32_t color = ft_pixel(rand() % 0xFF, // R
+										rand() % 0xFF, // G
+										rand() % 0xFF, // B
+										rand() % 0xFF  // A
 			);
 			mlx_put_pixel(image, i, y, color);
 		}
 	}
 }
 
-void ft_hook(void* param)
+void	ft_hook(void *param)
 {
-	mlx_t* mlx = param;
+	mlx_t	*mlx;
 
+	mlx = param;
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
 	if (mlx_is_key_down(mlx, MLX_KEY_UP))
@@ -65,32 +64,30 @@ void ft_hook(void* param)
 
 // -----------------------------------------------------------------------------
 
-int32_t main(int32_t argc, const char* argv[])
+int32_t	main(int32_t argc, const char *argv[])
 {
-	mlx_t* mlx;
+	mlx_t	*mlx;
 
 	// Gotta error check this stuff
 	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
 	{
 		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 	if (!(image = mlx_new_image(mlx, 128, 128)))
 	{
 		mlx_close_window(mlx);
 		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
 	{
 		mlx_close_window(mlx);
 		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
-	
 	mlx_loop_hook(mlx, ft_randomize, mlx);
 	mlx_loop_hook(mlx, ft_hook, mlx);
-
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
