f30babead10876a80e5f58d77e7df4d063d9e90c
AlexisNoukan <alexisnoukan@gmail.com>
2024-01-23 13:51:33 +0100

    norm

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -6,39 +6,19 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 11:45:33 by anoukan           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/01/23 13:17:36 by anoukan          ###   ########.fr       */
-=======
-/*   Updated: 2024/01/23 13:43:45 by anoukan          ###   ########.fr       */
->>>>>>> db27e2cd52431c9eb95b22daedf1dbb76d561df8
+/*   Updated: 2024/01/23 13:51:25 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-<<<<<<< HEAD
-//structure
-
-typedef struct s_map
-{
-    int fd;
-    char **map;
-    int E;
-    int P;
-    int W;
-} t_map;
-
 // include
-# include "../mlx42/include/MLX42/MLX42.h"
 # include "../LibFt/GNL/get_next_line.h"
-# include "../LibFt/printf/ft_printf.h"
 # include "../LibFt/libft/libft.h"
-=======
-// include
+# include "../LibFt/printf/ft_printf.h"
 # include "../mlx42/include/MLX42/MLX42.h"
 # include <fcntl.h>
->>>>>>> db27e2cd52431c9eb95b22daedf1dbb76d561df8
 # include <stdbool.h>
 # include <stdio.h>
 # include <stdlib.h>
