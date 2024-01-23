450a87501ed989b94b87710bc354f47fd49b28d7
AlexisNoukan <alexisnoukan@gmail.com>
2024-01-23 13:43:53 +0100

    norm

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -6,36 +6,36 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 11:45:33 by anoukan           #+#    #+#             */
-/*   Updated: 2024/01/23 12:22:30 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/01/23 13:43:45 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-//structure
-
-struct s_map
-{
-    int fd;
-    char **map;
-    int E;
-    int P;
-    int W;
-} t_map
-
 // include
 # include "../mlx42/include/MLX42/MLX42.h"
+# include <fcntl.h>
 # include <stdbool.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <fcntl.h>
+
+// structure
+
+typedef struct s_map
+{
+	int		fd;
+	char	**map;
+	int		e;
+	int		p;
+	int		w;
+}			t_map;
 
 // define
 # define WIDTH 256
 # define HEIGHT 256
 
 // window
-int32_t	ft_window(void);
+int			ft_window(void);
 
 #endif
