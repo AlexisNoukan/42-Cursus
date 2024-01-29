971f23b2e688775d790d8c798f0392bc3183fbff
AlexisNoukan <alexisnoukan@gmai.com>
2024-01-29 15:49:01 +0100

    qdd

--- so_long/main.c
+++ so_long/main.c
@@ -6,7 +6,7 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 14:52:13 by anoukan           #+#    #+#             */
-/*   Updated: 2024/01/23 14:28:53 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/01/29 10:11:32 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ int32_t	main(int argc, char **argv)
 	int		i;
 
 	if (argc < 2)
-		return (0);
+		return (ft_printf("no map found"), 0);
 	file = argv[1];
 	fd = open(file, O_RDONLY);
 	map.fd = fd;
-    i = 0;
+	i = 0;
 	while (i++ < 7)
 		ft_printf("%s", get_next_line(map.fd));
 	return (0);
--- so_long/srcs/parsing/parsing.c
+++ so_long/srcs/parsing/parsing.c
@@ -6,14 +6,36 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 10:51:58 by anoukan           #+#    #+#             */
-/*   Updated: 2024/01/23 13:45:40 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/01/29 15:11:30 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/so_long.h"
 
-int	ft_map_chequer(t_map)
+int	ft_verify_wall(t_map map)
 {
-	map.fd = 0;
+	size_t	i;
+
+	i = 0;
+	while(map->*map[i])
+}
+
+int	ft_valid(t_map map)
+{
+	if (map.e >= 1 && map.p == 1 && map.w == 1)
+		return (1);
 	return (0);
+}
+
+int	ft_map_chequer(t_map map)
+{
+	size_t	i;
+
+	i = 0;
+	while (get_next_line(map.fd) != 0)
+	{
+		map.map[i] = get_next_line(map.fd);
+		i++;
+	}
+	return (ft_valid(map));
 }
