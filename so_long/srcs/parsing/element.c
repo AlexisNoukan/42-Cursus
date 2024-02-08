d12e85a490271ab3999ef6d467e7b0855c54790a
AlexisNoukan <alexisnoukan@gmail.com>
2024-02-08 10:59:17 +0100

    so_long norm

--- so_long/srcs/parsing/element.c
+++ so_long/srcs/parsing/element.c
@@ -6,30 +6,30 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 10:49:51 by anoukan           #+#    #+#             */
-/*   Updated: 2024/02/08 10:56:15 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/02/08 10:58:56 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/so_long.h"
 
-int ft_verify_element(t_map map)
+int	ft_verify_element(t_map map)
 {
-    size_t  i;
-    size_t  j;
+	size_t	i;
+	size_t	j;
 
-    i = 0;
-    j = 0;
-    while(map[i][j])
-    {
-            while(map[i][j])
-    {
-          if (map[i][j] == e)
-            map.e = 1;
-        if (map[i][j] == p)
-            map.e = 1;
-            j++;
-    }
-      i++;
-    }
-    return (0);
+	i = 0;
+	j = 0;
+	while (map.map[i][j])
+	{
+		while (map.map[i][j])
+		{
+			if (map.map[i][j] == 'e')
+				map.e = 1;
+			if (map.map[i][j] == 'p')
+				map.e = 1;
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
