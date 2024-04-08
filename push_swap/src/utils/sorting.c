118dda08bc768813af71cf83a337faea41d04af0
AlexisNoukan <alexisnoukan@gmail.com>
2024-04-08 18:30:21 +0200

    add small sort

--- push_swap/src/utils/sorting.c
+++ push_swap/src/utils/sorting.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sorting.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
+/*   By: saliinger <saliinger@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 13:08:19 by anoukan           #+#    #+#             */
-/*   Updated: 2024/04/08 14:43:25 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/04/08 17:02:40 by saliinger        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,48 @@ bool	stack_sorted(t_stack **stack)
 
 void	small_sort(t_stack **stack_a, t_stack **stack_b)
 {
+	t_stack	*sorted;
+	t_stack	*current;
+	t_stack	*next;
+	t_stack	*temp;
+
+	sorted = NULL;
+	current = *stack_a;
+	while (current != NULL)
+	{
+		next = current->next;
+		if (sorted == NULL || sorted->number >= current->number)
+		{
+			current->next = sorted;
+			sorted = current;
+		}
+		else
+		{
+			temp = sorted;
+			while (temp->next != NULL && temp->next->number < current->number)
+			{
+				temp = temp->next;
+			}
+			current->next = temp->next;
+			temp->next = current;
+		}
+		current = next;
+	}
+	*stack_a = sorted;
+}
+
+void	big_sort(t_stack **stack_a, t_stack **stack_b)
+{
 	return ;
 }
 
 void	ft_sort(t_stack **stack_a, t_stack **stack_b)
 {
 	int	i;
-	int	n;
 
-	i = 0;
-	n = iterator(is_max(stack_a));
-	while (++i < n)
-	{
-	}
+	i = ft_lstsize(stack_a);
+	if (i > 10)
+		big_sort(stack_a, stack_b);
+	else
+		small_sort(stack_a, stack_b);
 }
