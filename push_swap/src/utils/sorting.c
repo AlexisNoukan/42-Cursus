dc8caf438ed5fe05289f74dcb8820c0b60dfa476
AlexisNoukan <alexisnoukan@gmai.com>
2024-04-09 14:36:17 +0200

    try make gfile

--- push_swap/src/utils/sorting.c
+++ push_swap/src/utils/sorting.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sorting.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saliinger <saliinger@student.42.fr>        +#+  +:+       +#+        */
+/*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 13:08:19 by anoukan           #+#    #+#             */
-/*   Updated: 2024/04/08 19:37:32 by saliinger        ###   ########.fr       */
+/*   Updated: 2024/04/09 14:36:14 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,9 +60,64 @@ void	small_sort(t_stack **stack_a, t_stack **stack_b)
 	*stack_a = sorted;
 }
 
+void	counting_sort(t_stack **stack_a, int exp)
+{
+	int		count[10] = {0};
+	t_stack	*output;
+	t_stack	*current;
+	t_stack	*next;
+	int		index;
+	t_stack	*temp;
+
+	if (*stack_a == NULL)
+		return ;
+	output = NULL;
+	current = *stack_a;
+	while (current != NULL)
+	{
+		count[(current->number / exp) % 10]++;
+		current = current->next;
+	}
+	for (int i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+	current = *stack_a;
+	while (current != NULL)
+	{
+		next = current->next;
+		index = (current->number / exp) % 10;
+		temp = (t_stack *)malloc(sizeof(t_stack));
+		if (temp == NULL)
+		{
+			printf("Memory allocation failed\n");
+			exit(1);
+		}
+		temp->number = current->number;
+		temp->next = NULL;
+		if (output == NULL)
+			output = temp;
+		else
+		{
+			temp->next = output;
+			output = temp;
+		}
+		current = next;
+	}
+	*stack_a = output;
+}
+
 void	big_sort(t_stack **stack_a, t_stack **stack_b)
 {
-	return ;
+	int	max;
+
+	if (*stack_a == NULL)
+		return ;
+	max = is_max(*stack_q);
+	for (int exp = 1; max / exp > 0; exp *= 10)
+	{
+		countingSort(stack_a, exp);
+		while ((*stack_a)->number / exp == 0)
+			ra(stack_q);
+	}
 }
 
 void	ft_sort(t_stack **stack_a, t_stack **stack_b)
