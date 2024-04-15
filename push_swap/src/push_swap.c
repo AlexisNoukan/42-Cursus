aef092259d49cfa5e939e7d5e5c469090ee06121
AlexisNoukan <alexisnoukan@gmai.com>
2024-04-15 15:33:02 +0200

    push issue with ft_print(a ) and maybe the sort

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 09:34:52 by anoukan           #+#    #+#             */
-/*   Updated: 2024/04/15 14:24:36 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/04/15 15:32:19 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,26 @@ void	ft_init(char **argv, t_stack **a)
 	char	**dest;
 	int		i;
 	t_stack	*new;
+	int		j;
 
 	dest = ft_split(argv[1], ' ');
 	i = 0;
+	j = ft_atoi(dest[i]);
+	printf("j init : %d\n", j);
 	while (dest[i])
 	{
-		new = ft_lstnew2(ft_atoi(dest[i]));
+		j = ft_atoi(dest[i]);
+		new = ft_lstnew2(j);
 		ft_lstadd_back2(a, new);
 		i++;
+		printf("j loop : %d\n", j);
 		free(new);
 	}
+	printf("init done\n");
 	i = 0;
 	while (dest[i])
 	{
+		printf("dest free %s\n", dest[i]);
 		free(dest[i]);
 		i++;
 	}
@@ -41,7 +48,6 @@ int	main(int argc, char **argv)
 	t_stack	**a;
 	t_stack	**b;
 
-	ft_printf("%s\n", argv[1]);
 	a = (t_stack **)malloc(sizeof(t_stack **));
 	b = (t_stack **)malloc(sizeof(t_stack **));
 	if (argc < 2)
@@ -49,8 +55,11 @@ int	main(int argc, char **argv)
 	else if (argc != 2)
 		return (0);
 	ft_init(argv, a);
-	if (!stack_sorted(a))
+	ft_printf("initialized\n");
+	ft_print(a);
+	if (stack_sorted(a) == false)
 		ft_sort(a, b);
+	ft_printf("sorted\n");
 	ft_printf("Stack A: ");
 	ft_print(a);
 	ft_printf("Stack B: ");
--- push_swap/src/utils/sorting.c
+++ push_swap/src/utils/sorting.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sorting.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saliinger <saliinger@student.42.fr>        +#+  +:+       +#+        */
+/*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 13:08:19 by anoukan           #+#    #+#             */
-/*   Updated: 2024/04/12 16:38:35 by saliinger        ###   ########.fr       */
+/*   Updated: 2024/04/15 14:55:38 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,24 @@ bool	stack_sorted(t_stack **stack)
 {
 	t_stack	*temp;
 
+	ft_printf("sorting\n");
 	if (!stack)
 		return (-1);
 	temp = *stack;
 	while (temp->next)
 	{
 		if (temp->number > temp->next->number)
-			return (false);
+			return (ft_printf("false\n"), false);
 		temp = temp->next;
 	}
-	return (true);
+	return (ft_printf("true\n"), true);
 }
 
 void	counting_sort(t_stack **stack_a, t_stack **stack_b)
 {
-	while ((*stack_b)->next != NULL)
+	while ((*stack_b)->next != NULL || *stack_b != NULL)
 	{
-		while ((*stack_a)->next != NULL)
+		while ((*stack_a)->next != NULL || *stack_a != NULL)
 		{
 			if ((*stack_a)->number - (*stack_b)->number > 0)
 				*stack_a = (*stack_a)->next;
@@ -53,6 +54,7 @@ void	ft_sort(t_stack **stack_a, t_stack **stack_b)
 	if (*stack_a == NULL)
 		return ;
 	meanvalue = mean_value(stack_a);
+	ft_printf("%d\n", mean_value);
 	while ((*stack_a)->next != NULL && ft_lstsize2(*stack_a) >= 5)
 	{
 		if ((*stack_a)->number <= meanvalue)
@@ -62,5 +64,6 @@ void	ft_sort(t_stack **stack_a, t_stack **stack_b)
 		*stack_a = (*stack_a)->next;
 		meanvalue = mean_value(stack_a);
 	}
+	ft_printf("%d", stack_a[1]);
 	counting_sort(stack_a, stack_b);
 }
