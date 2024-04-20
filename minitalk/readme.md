01f88e22d0b8fa48931a498067aa41b986866025
AlexisNoukan <alexisnoukan@gmail.com>
2024-04-20 23:30:24 +0200

    add base

---
+++ minitalk/readme.md
@@ -0,0 +1,11 @@
+# client
+- 1 char = 8bit
+- to send 1 char need to send 8 signal with kill
+
+- need to send the pid 
+
+# server
+- to reiceive 1 char need to wait for the 8 sig
+- use of printf to show the message
+
+- print client and server pid
--- minitalk/src/client/client.c
+++ minitalk/src/client/client.c
@@ -6,26 +6,30 @@
 /*   By: saliinger <saliinger@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 16:43:20 by saliinger         #+#    #+#             */
-/*   Updated: 2024/04/20 22:35:37 by saliinger        ###   ########.fr       */
+/*   Updated: 2024/04/20 23:29:16 by saliinger        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-	#include "../../includes/minitalk.h"
+#include "../../includes/minitalk.h"
 
 int	main(int argc, char **argv)
 {
 	int	pid;
+	int	my_pid;
 
 	ft_printf(" ██████╗██╗     ██╗███████╗███╗   ██╗████████╗\n");
 	ft_printf("██╔════╝██║     ██║██╔════╝████╗  ██║╚══██╔══╝\n");
 	ft_printf("██║     ██║     ██║█████╗  ██╔██╗ ██║   ██║   \n");
 	ft_printf("██║     ██║     ██║██╔══╝  ██║╚██╗██║   ██║   \n");
 	ft_printf("╚██████╗███████╗██║███████╗██║ ╚████║   ██║   \n");
-	ft_printf(" ╚═════╝╚══════╝╚═╝╚══════╝╚═╝  ╚═══╝   ╚═╝   \n");
+	ft_printf(" ╚═════╝╚══════╝╚═╝╚══════╝╚═╝  ╚═══╝   ╚═╝   \n\n\n");
 	if (argc != 3)
 		return (EXIT_FAILURE);
 	pid = ft_atoi(argv[1]);
-	ft_printf("pid : %d\n", pid);
-	ft_printf("Message : \n%s", argv[2]);
+	my_pid = getpid();
+	ft_printf("pid : %d\nmy pid : %d\n\n", pid, my_pid);
+	ft_printf("Message : \n\n%s", argv[2]);
+	while (1)
+		sleep(1);
 	return (0);
 }
