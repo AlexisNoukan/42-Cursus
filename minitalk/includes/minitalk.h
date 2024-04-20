3deb4fb2792fc72d1dab19444bf42f35f96d2255
AlexisNoukan <alexisnoukan@gmail.com>
2024-04-20 16:18:12 +0200

    init minitalk

---
+++ minitalk/includes/minitalk.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saliinger <saliinger@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/20 16:16:20 by saliinger         #+#    #+#             */
+/*   Updated: 2024/04/20 16:18:01 by saliinger        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+// ---Lib---
+# include <sys/types.h>
+# include <unistd.h>
+# include "../libft/libft.h"
+
+//---Server---
+
+//---Client---
+
+#endif
