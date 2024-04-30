433360dd07774a8a659541894e800c70b27cbbb9
AlexisNoukan <alexisnoukan@gmai.com>
2024-04-30 16:59:04 +0200

    dont give a f

--- minitalk/src/server/ft_handler.c
+++ minitalk/src/server/ft_handler.c
@@ -6,7 +6,7 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 12:26:38 by anoukan           #+#    #+#             */
-/*   Updated: 2024/04/30 14:53:15 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/04/30 16:49:56 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,14 @@ void	server_handler(int signum, siginfo_t info, void *context)
 {
 	pid_t		client_pid;
 	static char	current_char;
+	int			bit;
 
 	client_pid = info.si_pid;
+    bit = 0;
+    while (bit < 8)
+    {
+        bit++;
+        c >>= 1;
+    }
+    
 }
